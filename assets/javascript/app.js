b1a52e7c0252b768146a5ec7ffec78c2a5088f1a
unknown <mattcmej@gmail.com>
2018-03-30 17:20:38 -0700

    Create test layout for webpage.

---
+++ assets/javascript/app.js
@@ -0,0 +1,18 @@
+/*
+
+Game Plan:
+
+1. Create divs on webpage
+    a. Section above the gifs for rating
+    b. Section for the gifs
+
+2. Make the buttons display gifs corresponding to the button labels
+    a. e.g. If the button is labeled "cats," display cats.
+
+3. Every time a button is pressed, clear out the old divs and add in the new ones corresponding to the pressed button
+
+4. Create a way that allows the user to create their own buttons dynamically.
+
+
+*/
+
