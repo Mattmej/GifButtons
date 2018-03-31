f535b12814edac1b17e5f59a6b5f27cba4be1ae2
unknown <mattcmej@gmail.com>
2018-03-30 18:12:26 -0700

    Attach functionality to buttons.

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -16,3 +16,51 @@ Game Plan:
 
 */
 
+$("button").on("click", function() {
+
+    // this variable will store whatever the "data-animal" attribute is set to 
+    var animal = $(this).attr("data-animal");
+    
+
+    // create a variable to hold the queryURL.
+    var queryURL = "https://api.giphy.com/v1/gifs/search?q=" + animal + "&api_key=pgNlFjIaMhsyZr48vV7fIJOHNC7r3sQG&limit=10";
+
+
+
+    // ajax call to retrieve data from the queryURL
+    $.ajax({
+        url: queryURL,
+        method: "GET"
+    })
+
+    .then(function(response) {
+
+        var results = response.data;
+
+        for (i = 0; i < results.length; i++) {
+
+
+            // create new divs for every result
+            // This will hold the rating and the image.
+            var animalResult = $("<div class = 'd-flex flex-column'></div>");
+
+            var animalRating = $("<p>").text("Rating: " + results[i].rating);
+
+            var animalImage = $("<img>");
+            animalImage.attr("src", results[i].images.fixed_height.url);
+
+            $("#results").append(animalResult);
+            $(animalResult).append(animalRating);
+            $(animalResult).append(animalImage);
+
+
+        }
+
+
+    })
+
+
+
+
+
+})
